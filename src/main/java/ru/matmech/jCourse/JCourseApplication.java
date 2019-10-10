5972247b9636fd8db2067862f019b1745354d93a
Resinchen <alex1410alex@gmail.com>
2019-10-11 00:28:20 +0500

    upload project

---
+++ src/main/java/ru/matmech/jCourse/JCourseApplication.java
@@ -0,0 +1,13 @@
+package ru.matmech.jCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JCourseApplication.class, args);
+	}
+
+}
